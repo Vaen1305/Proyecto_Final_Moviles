57b087e4687fb3b11d5fa70faf186497e9b4442c
JomaPopo <150464590+JomaPopo@users.noreply.github.com>
2025-05-20 16:12:55 -0500

    JomaBase

---
+++ Assets/ScriptsPruebaLav10/CharacterData.cs
@@ -0,0 +1,15 @@
+using Firebase.Firestore;
+using UnityEngine;
+
+[FirestoreData]
+public struct CharacterData
+{
+    [FirestoreProperty]
+    public string Name { get; set; }
+    [FirestoreProperty]
+    public string Description { get; set; }
+    [FirestoreProperty]
+    public int Attack { get; set; }
+    [FirestoreProperty]
+    public int Defense { get; set; }
+}
